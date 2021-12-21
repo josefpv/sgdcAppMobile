adc84c8af41dc848b8d36b2e2e769dc8151854e6
Jose <vivas.f13@gmail.com>
2021-12-21 20:18:02 -0300

    first commit.

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+//React Native Elements
+import { SafeAreaProvider } from "react-native-safe-area-context";
+//Fonts
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "titilliumWeb-regular": require("./assets/fonts/TitilliumWeb-Regular.ttf"),
+    "titilliumWeb-bold": require("./assets/fonts/TitilliumWeb-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onError={() => console.log("error")}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/Marquesinas.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native-elements";
+
+const Marquesinas = () => {
+  return (
+    <>
+      <Text>Marquesinas</Text>
+    </>
+  );
+};
+
+export default Marquesinas;
